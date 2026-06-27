c47fca4149490910c439aea807fb50d38b14e90f
ShivGrover45 <codershiv16@gmail.com>
2026-06-27 22:03:50 +0530

    api endpoints for app

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI,UploadFile,File
+from fastapi.middleware.cors import CORSMiddleware
+from ingestor import load_and_split
+from retriever import ingest_documents, retrieve
+from generator import generate_answer
 
-app=FastAPI()
+app=FastAPI(title="Veda AI",version="1.0.0")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...)):
+    contents=await file.read()
+    temp_path=f"tem_{file.filename}"
+    with open(temp_path,"wb") as f:
+        f.write(contents)
+    chunks=load_and_split(temp_path)
+    ingest_documents(chunks)
+    os.remove(temp_path)
+    return {"message": f"File '{file.filename}' uploaded and processed successfully."}
+
+@app.post("/query")
+async def student_query(payload: dict):
+    query = payload.get("query")
+    if not query:
+        return {"error": "Query not provided."}
+    results = retrieve(query)
+    answer=generate_answer(query, results)
+    return {"answer":answer}
